bdf896487445664d7a094b41d729a0e0e2d58d2b
Kat Marchán <kzm@sykosomatic.org>
2009-12-30 01:18:24 -0600

    Exporting symbols.

--- src/package.lisp
+++ src/package.lisp
@@ -1,3 +1,43 @@
 (cl:defpackage #:chillax
   (:use :cl :sheeple :flexi-streams :drakma)
-  (:nicknames :chill))
+  (:nicknames :chill)
+  (:export
+
+   ;; Conditions
+   :couchdb-error :unexpected-response :database-error
+   :db-not-found :db-already-exists :document-error
+   :document-not-found :document-conflict
+
+   ;; Server API
+   :=server=
+   :host :port :username :password
+   :=json-server=
+   :couch-request
+   :all-dbs
+   :config-info
+   :replicate
+   :stats
+   :active-tasks
+   :get-uuids
+
+   ;; Database API
+   :=database=
+   :server :name
+   :db-request
+   :db-info
+   :connect-to-db
+   :create-db
+   :ensure-db
+   :delete-db
+   :compact-db
+   :changes
+
+   ;; Documents API
+   :get-document
+   :all-documents
+   :batch-get-documents
+   :put-document
+   :post-document
+   :delete-document
+   :copy-document
+   ))
