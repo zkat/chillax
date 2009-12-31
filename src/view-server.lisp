cc23f58f130724984563b60d38f8b3751f4b8f70
Kat Marchán <kzm@sykosomatic.org>
2009-12-31 01:43:04 -0600

    Wrote simple lisp-based view server.

---
+++ src/view-server.lisp
@@ -0,0 +1,64 @@
+(defpackage #:chillax-server (:use :cl))
+(defpackage #:chillax-server-user (:use :cl))
+(in-package :chillax-server)
+
+(defvar *map-results*)
+(defvar *functions*)
+
+(defun equal-hash (&rest keys &aux (table (make-hash-table :test #'equal)))
+  (loop for (key val) on keys by #'cddr do (setf (gethash key table) val)
+     finally (return table)))
+
+(defun emit (k v)
+  (when (boundp *map-results*)
+    (push (list k v) *map-results*)))
+
+(defmacro with-user-package (&body body)
+  `(let ((*package* (find-package :chillax-server-user)))
+     ,@body))
+
+(defun couch-log (format-string &rest format-args)
+  (format t "[\"log\", ~S]~%" (apply #'format nil format-string format-args))
+  (finish-output))
+
+(defun add-fun (string)
+  (handler-case (prog1 t (push (eval (read-from-string string)) *functions*))
+    (error (err)
+      (equal-hash "error" "function_compilation_failed"
+                  "reason" (princ-to-string (type-of err))))))
+
+(defun reset () (setf *functions* nil) t)
+
+(defun call-map-function (function doc &aux *map-results*)
+  (with-user-package (funcall function doc)) (or *map-results* '(#())))
+
+(defun map-doc (doc)
+  (or (mapcar (lambda (fn) (call-map-function fn doc)) *functions*) '((#()))))
+
+(defun reduce-map (fun-strings keys values)
+  (list t (mapcar (lambda (fn) (with-user-package
+                                 (funcall (eval (read-from-string fn)) keys values)))
+                  fun-strings)))
+
+(defun rereduce (fun-strings reduce-results)
+  (list t (mapcar (lambda (fn) (with-user-package
+                                 (funcall (eval (read-from-string fn)) reduce-results)))
+                  fun-strings)))
+
+(defun run-server (&aux *functions*)
+  (handler-case
+      (loop for input = (read-line)
+         for parsed = (json:parse input)
+         do (json:encode (cond ((string= (car parsed) "reset") (reset))
+                               ((string= (car parsed) "add_fun") (add-fun (cadr parsed)))
+                               ((string= (car parsed) "map_doc") (map-doc (cadr parsed)))
+                               ((string= (car parsed) "reduce")
+                                (loop for result in (third parsed)
+                                   collect (caar result) into keys
+                                   collect (cadr result) into values
+                                   finally (return (reduce-map (second parsed) keys values))))
+                               ((string= (car parsed) "rereduce") (apply #'rereduce (cdr parsed)))
+                               (t (error "Unknown message: ~S" input))))
+         (terpri)
+         (finish-output))
+    (end-of-file () nil)))
