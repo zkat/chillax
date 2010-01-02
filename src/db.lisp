06422700dcc1012aeea6251dfaf84f5044a60b30
Kat Marchán <kzm@sykosomatic.org>
2010-01-02 11:59:58 -0600

    Adding hash table utils to db.lisp

--- src/db.lisp
+++ src/db.lisp
@@ -5,6 +5,26 @@
   "Concatenates a series of strings."
   (apply 'concatenate 'string string more-strings))
 
+(defun mkhash (&rest keys-and-values &aux (table (make-hash-table :test #'equal)))
+  "Convenience function for `literal' hash table definition."
+  (loop for (key val) on keys-and-values by #'cddr do (setf (gethash key table) val)
+     finally (return table)))
+
+(defun hashget (hash key &rest more-keys)
+  "Convenience function for recursively accessing hash tables."
+  (flet ((reverse-gethash (hash key) (gethash key hash)))
+    (reduce #'reverse-gethash more-keys :initial-value (gethash key hash))))
+
+(defun (setf hashget) (new-value hash key &rest more-keys)
+  "Uses the last key given to hashget to insert NEW-VALUE into the hash table
+returned by the second-to-last key.
+tl;dr: DWIM SETF function for HASHGET."
+  (if more-keys
+      (setf (gethash (car (last more-keys))
+                     (apply #'hashget hash key (butlast more-keys)))
+            new-value)
+      (setf (gethash key hash) new-value)))
+
 ;;;
 ;;; Status codes
 ;;;
