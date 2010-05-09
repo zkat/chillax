45481fe1b6960010c5c67e60419c71676ec63897
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2010-05-10 06:03:47 +0800

    initial BROKEN commit for chillax-clos

---
+++ src/package-clos.lisp
@@ -0,0 +1,43 @@
+(cl:defpackage #:chillax-clos
+  (:use :cl :flexi-streams :drakma)
+  (:nicknames :chill-c)
+  (:export
+
+   ;; Conditions
+   :couchdb-error :unexpected-response :database-error
+   :db-not-found :db-already-exists :document-error
+   :document-not-found :document-conflict
+
+   ;; Server API
+   :server
+   :host :port :username :password
+   :json-server
+   :couch-request
+   :all-dbs
+   :config-info
+   :replicate
+   :stats
+   :active-tasks
+   :get-uuids
+
+   ;; Database API
+   :database
+   :server :name
+   :db-request
+   :db-info
+   :connect-to-db
+   :create-db
+   :ensure-db
+   :delete-db
+   :compact-db
+   :changes
+
+   ;; Documents API
+   :get-document
+   :all-documents
+   :batch-get-documents
+   :put-document
+   :post-document
+   :delete-document
+   :copy-document
+   ))
