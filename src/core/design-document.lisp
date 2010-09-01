66f23fa0e9a067846c3c006e840d833ae1d88d74
Josh Marchán <sykopomp@sykosomatic.org>
2010-09-01 00:35:21 -0500

    Settling on simpler design doc API for now.

--- src/core/design-document.lisp
+++ src/core/design-document.lisp
@@ -17,46 +17,45 @@
     (:ok response)
     (:not-found (error 'document-not-found :db db :id design-doc-name))))
 
+(defun invoke-design-doc (db design-doc-name &rest all-keys)
+  (apply #'get-document db (strcat "_design/" design-doc-name) all-keys))
+
 ;;;
 ;;; Views
 ;;;
-(defgeneric view-map-definition (view)
-  (:method ((view hash-table))
-    (at view "map")))
-(defgeneric view-reduce-definition (view)
-  (:method ((view hash-table))
-    (at view "reduce")))
-
-(defun get-temporary-view (db view &optional (language "common-lisp"))
+(defun get-temporary-view (db &key
+                           (language "common-lisp")
+                           (map (error "Must provide a map function for temporary views."))
+                           reduce)
   (let ((json (with-output-to-string (s)
                 (format s "{")
-                (format s "\"map\":~S" (view-map-definition view))
-                (when (view-reduce-definition view)
-                  (format s ",\"reduce\":~S" (view-reduce-definition view)))
                 (format s ",\"language\":~S" language)
+                (format s "\"map\":~S" map)
+                (when reduce
+                  (format s ",\"reduce\":~S" reduce))
                 (format s "}"))))
     (handle-request (response db "_temp_view" :method :post
                               :content json
                               :convert-data-p nil)
       (:ok response))))
 
-;;;
-;;; Unstable
-;;;
-(defun make-view (map-def &optional reduce-def)
-  (let ((view (mkhash "map" map-def)))
-    (when reduce-def (setf (at view "reduce") reduce-def))
-    view))
+;; (defun make-view (map-def &optional reduce-def)
+;;   (let ((view (mkhash "map" map-def)))
+;;     (when reduce-def (setf (at view "reduce") reduce-def))
+;;     view))
 
-(defun ensure-design-doc (&optional (language "common-lisp"))
-  (mkhash "language" language))
+;; (defun ensure-design-doc (db name &key (language "common-lisp") views
+;;                           &aux (full-name (strcat "_design/" name)))
+;;   (let ((old-doc (get-document db full-name))
+;;         (new-doc (mkhash "language" language
+;;                          "views" views)))
+;;     (when old-doc
+;;       (setf (at new-doc "_rev") (at old-doc "_rev")))
+;;     (put-document db full-name new-doc)
+;;     full-name))
 
-(defun ensure-view (design-doc view-name view)
-  (let ((existing-views (at design-doc "views")))
-    (if existing-views
-        (setf (at design-doc "views" view-name) view)
-        (setf (at design-doc "views") (mkhash view-name view))))
-  design-doc)
-
-(defun save-design-doc (db name design-doc)
-  (put-document db (strcat "_design/" name) design-doc))
+;; (defun ensure-view (design-doc view-name view)
+;;   (if (at design-doc "views")
+;;       (setf (at design-doc "views" view-name) view)
+;;       (setf (at design-doc "views") (mkhash view-name view)))
+;;   design-doc)
