e17220fe69ebf0e6116e6ee778f74cae4c02e3e3
Josh Marchán <sykopomp@sykosomatic.org>
2010-11-23 23:31:16 -0600

    Document API now uses functions instead of genfuns.

--- src/core/document.lisp
+++ src/core/document.lisp
@@ -25,67 +25,59 @@
 ;;;
 ;;; Direct Document API
 ;;;
-(defgeneric get-document (db id &key)
-  (:documentation "Returns an CouchDB document from DB as an alist.")
-  (:method (db id &key key startkey endkey limit include-docs &aux params)
-    (flet ((add-param (key value)
-             (push (cons key (prin1-to-string value)) params)))
-      (when key (add-param "key" key))
-      (when startkey (add-param "startkey" startkey))
-      (when endkey (add-param "endkey" endkey))
-      (when limit (add-param "limit" limit))
-      (when include-docs (add-param "include_docs" "true"))
-      (handle-request (response db (princ-to-string id) :parameters params)
-        (:ok response)
-        (:not-found (error 'document-not-found :db db :id id))))))
-
-(defgeneric all-documents (db &key)
-  (:documentation "Returns all CouchDB documents in DB, in alist form.")
-  (:method (db &rest all-keys)
-    (apply #'get-document db "_all_docs" all-keys)))
-
-(defgeneric batch-get-documents (db &rest doc-ids)
-  (:documentation
-   "Uses _all_docs to quickly fetch the given DOC-IDs in a single request. Note that this function
-   will NOT signal a DOCUMENT-NOT-FOUND error when one or more DOC-IDs are not found. Instead, the
-   results will be returned, and it's the user's responsibility to deal with any missing docs.")
-  (:method (db &rest doc-ids)
-    (handle-request (response db "_all_docs" :method :post
-                              :parameters '(("include_docs" . "true"))
-                              :content (format nil "{\"keys\":[~{~S~^,~}]}" doc-ids)
-                              :convert-data-p nil)
-      (:ok response))))
-
-(defgeneric put-document (db id doc &key batch-ok-p)
-  (:documentation "Puts a document into DB, using ID.")
-  (:method (db id doc &key batch-ok-p)
-    (handle-request (response db (princ-to-string id) :method :put :content doc
-                              :parameters (when batch-ok-p '(("batch" . "ok"))))
-      ((:created :accepted) response)
-      (:conflict (error 'document-conflict :id id :doc doc)))))
-
-(defgeneric post-document (db doc)
-  (:documentation
-   "POSTs a document into DB. CouchDB will automatically assign a UUID if the document does not
-   already exist. Note that using this function is discouraged in the CouchDB documentation, since
-   it may result in duplicate documents because of proxies and other network intermediaries.")
-  (:method (db doc)
-    (handle-request (response db "" :method :post :content doc)
-      ((:created :accepted) response)
-      (:conflict (error 'document-conflict :doc doc)))))
-
-(defgeneric delete-document (db id revision)
-  (:documentation "Deletes an existing document.")
-  (:method (db id revision)
-    (handle-request (response db (format nil "~A?rev=~A" id revision) :method :delete)
+(defun get-document (db id &key key startkey endkey limit include-docs &aux params)
+  "Finds a CouchDB document in DB, named by ID."
+  (flet ((add-param (key value)
+           (push (cons key (prin1-to-string value)) params)))
+    (when key (add-param "key" key))
+    (when startkey (add-param "startkey" startkey))
+    (when endkey (add-param "endkey" endkey))
+    (when limit (add-param "limit" limit))
+    (when include-docs (add-param "include_docs" "true"))
+    (handle-request (response db (princ-to-string id) :parameters params)
       (:ok response)
       (:not-found (error 'document-not-found :db db :id id)))))
 
-(defgeneric copy-document (db from-id to-id &key)
-  (:documentation "Copies a document's content in-database.")
-  (:method (db from-id to-id &key revision)
-    (handle-request (response db (princ-to-string from-id) :method :copy
-                              :additional-headers `(("Destination" . ,(princ-to-string to-id)))
-                              :parameters `(,(when revision `("rev" . ,revision))))
-      (:created response)
-      (:not-found (error 'document-not-found :db db :id from-id)))))
+(defun all-documents (db &rest all-keys)
+  "Requests the \_all\_docs document. ALL-KEYS correspond to GET-DOCUMENT's keyword arguments."
+  (apply #'get-document db "_all_docs" all-keys))
+
+(defun batch-get-documents (db &rest doc-ids)
+  "Uses _all_docs to quickly fetch the given DOC-IDs in a single request. Note that this function
+will NOT signal a DOCUMENT-NOT-FOUND error when one or more DOC-IDs are not found. Instead, the
+results will be returned, and it's the user's responsibility to deal with any missing docs."
+  (handle-request (response db "_all_docs" :method :post
+                            :parameters '(("include_docs" . "true"))
+                            :content (format nil "{\"keys\":[~{~S~^,~}]}" doc-ids)
+                            :convert-data-p nil)
+    (:ok response)))
+
+
+(defun put-document (db id doc &key batch-ok-p)
+  "Puts a document into DB, using ID."
+  (handle-request (response db (princ-to-string id) :method :put :content doc
+                            :parameters (when batch-ok-p '(("batch" . "ok"))))
+    ((:created :accepted) response)
+    (:conflict (error 'document-conflict :id id :doc doc))))
+
+(defun post-document (db doc)
+  "POSTs a document into DB. CouchDB will automatically assign a UUID if the document does not
+already exist. Note that using this function is discouraged in the CouchDB documentation, since it
+may result in duplicate documents because of proxies and other network intermediaries."
+  (handle-request (response db "" :method :post :content doc)
+    ((:created :accepted) response)
+    (:conflict (error 'document-conflict :doc doc))))
+
+(defun delete-document (db id revision)
+  "Deletes an existing document."
+  (handle-request (response db (format nil "~A?rev=~A" id revision) :method :delete)
+    (:ok response)
+    (:not-found (error 'document-not-found :db db :id id))))
+
+(defun copy-document (db from-id to-id &key revision)
+  "Copies a document's content in-database."
+  (handle-request (response db (princ-to-string from-id) :method :copy
+                            :additional-headers `(("Destination" . ,(princ-to-string to-id)))
+                            :parameters `(,(when revision `("rev" . ,revision))))
+    (:created response)
+    (:not-found (error 'document-not-found :db db :id from-id))))
