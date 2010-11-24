fc0b07869d84c24100da29f93cb9192840429c84
Josh Marchán <sykopomp@sykosomatic.org>
2010-11-23 22:07:30 -0600

    Formatting change.

--- src/yason.lisp
+++ src/yason.lisp
@@ -9,18 +9,23 @@
 (defgeneric parse-object-key-fun (server))
 
 (defclass yason-server (standard-server)
-  ((array-as-vector-p :initarg :array-as-vector-p
-                      :reader parse-json-arrays-as-vectors-p)
-   (boolean-as-symbol-p :initarg :boolean-as-symbol-p
-                        :reader parse-json-booleans-as-symbols-p)
-   (object-as-alist-p :initarg :object-as-alist-p
-                      :reader parse-object-as-alist-p)
-   (parse-object-key-fun :initarg :parse-object-key-fun
-                         :reader parse-object-key-fun))
+  ((array-as-vector-p
+    :initarg :array-as-vector-p
+    :reader parse-json-arrays-as-vectors-p)
+   (boolean-as-symbol-p
+    :initarg :boolean-as-symbol-p
+    :reader parse-json-booleans-as-symbols-p)
+   (object-as-alist-p
+    :initarg :object-as-alist-p
+    :reader parse-object-as-alist-p)
+   (parse-object-key-fun
+    :initarg :parse-object-key-fun
+    :reader parse-object-key-fun))
   (:documentation
    "YASON-SERVERs use Yason's JSON parser/encoder to automatically translate content going to/coming
    from the associated CouchDB server.")
-  (:default-initargs :array-as-vector-p nil
+  (:default-initargs
+      :array-as-vector-p nil
     :boolean-as-symbol-p nil
     :object-as-alist-p nil
     :parse-object-key-fun #'identity))
